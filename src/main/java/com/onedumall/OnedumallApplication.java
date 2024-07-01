0b25b2df111ce31d91ffeb82f2a7768eea7fd9d2
Wonki11 <wongi5011@daum.net>
2024-07-01 16:02:14 +0900

    OneDuMall Start

---
+++ src/main/java/com/onedumall/OnedumallApplication.java
@@ -0,0 +1,13 @@
+package com.onedumall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnedumallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnedumallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onedumall/OnedumallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onedumall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnedumallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
