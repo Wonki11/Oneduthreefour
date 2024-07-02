413085daf51fdc91100d4b463b16c98cd8363bee
Wonki11 <wongi5011@daum.net>
2024-07-02 09:50:11 +0900

    mapper url modify

--- src/main/java/com/onedumall/controller/MypageMainController.java
+++ src/main/java/com/onedumall/controller/MypageMainController.java
@@ -21,38 +21,12 @@ public class MypageMainController {
 	@Autowired
 	private MyInfoService myinfoService;
 
-	@GetMapping("/login")
-	public String showLoginForm(Model model) {
-		model.addAttribute("m", new Members());
-		return "login";
-	}
 
 	@GetMapping("/mypage_home")
 	public String MypageHome(HttpSession session, Model model) {
 		return "mypage_home";
 	}
 
-	@PostMapping("/login")
-	public String getLogin(Model model, @RequestParam("members_id") String members_id,
-			@RequestParam("members_pw") String members_pw, HttpSession session) {
-
-		Members members = myinfoService.getLogin(members_id, members_pw);
-
-		if (members != null) { // member 객체에 값이 들어있다면!
-			session.setAttribute("loginSession", members);
-			return "mypage_home"; // 로그인 성공하면 메인 페이지로 보내기
-		} else { // 일치하는 정보가 없다면
-			model.addAttribute("error", "일치하는 아이디 비밀번호가 없습니다.");
-			model.addAttribute("m", new Members());
-			return "login";// 다시 로그인 입력창으로 돌려보내기
-		}
-	}
-
-	@GetMapping("/logout")
-	public String logout(HttpSession session) {
-		session.invalidate();// invalidate 무효시키다. 없던일로만들다.
-		return "redirect:/";
-	}
 
 	/* ********************************************************************** */
 
